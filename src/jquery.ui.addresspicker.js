64845e868e3e22ed665678900cabf956fdb9d9aa
adagyo <alexandre.courtois@gmail.com>
2013-02-13 15:08:42 +0100

    Can now display all address_components given by google maps API

--- src/jquery.ui.addresspicker.js
+++ src/jquery.ui.addresspicker.js
@@ -1,11 +1,8 @@
 /*
  * jQuery UI addresspicker @VERSION
  *
- * Copyright 2010, AUTHORS.txt (http://jqueryui.com/about)
  * Dual licensed under the MIT or GPL Version 2 licenses.
  * http://jquery.org/license
- *
- * http://docs.jquery.com/UI/Progressbar
  *
  * Depends:
  *   jquery.ui.core.js
@@ -20,22 +17,57 @@
 			regionBias: null,
 			mapOptions: {
 				zoom: 5, 
-				center: new google.maps.LatLng(46, 2), 
+				center: new google.maps.LatLng(45.889042, 6.23809499999993),
 				scrollwheel: false,
 				mapTypeId: google.maps.MapTypeId.ROADMAP
 			},
 			elements: {
 				map: false,
-				lat: false,
-				lng: false,
-				street_number: false,
-				route: false,
-				locality: false,
-				administrative_area_level_2: false,
-				administrative_area_level_1: false,
-				country: false,
-				postal_code: false,
-				type: false
+                lat: false,
+                lng: false,
+                type: false,
+                long_names: {
+                    street_number: false,
+                    route: false,
+                    intersection: false,
+                    locality: false,
+                    political: false,
+                    administrative_area_level_3: false,
+                    administrative_area_level_2: false,
+                    administrative_area_level_1: false,
+                    country: false,
+                    postal_code: false,
+                    colloquial_area: false,
+                    sublocality: false,
+                    neighborhood: false,
+                    premise: false,
+                    subpremise: false,
+                    natural_feature: false,
+                    airport: false,
+                    park: false,
+                    point_of_interest: false
+                },
+                short_names : {
+                    street_number: false,
+                    route: false,
+                    intersection: false,
+                    locality: false,
+                    political: false,
+                    administrative_area_level_3: false,
+                    administrative_area_level_2: false,
+                    administrative_area_level_1: false,
+                    country: false,
+                    postal_code: false,
+                    colloquial_area: false,
+                    sublocality: false,
+                    neighborhood: false,
+                    premise: false,
+                    subpremise: false,
+                    natural_feature: false,
+                    airport: false,
+                    park: false,
+                    point_of_interest: false
+                }
 			}
 		},
 
@@ -68,19 +100,55 @@
 				focus:  $.proxy(this._focusAddress, this),
 				select: $.proxy(this._selectAddress, this)
 			});
-		  
-			this.lat      = $(this.options.elements.lat);
-			this.lng      = $(this.options.elements.lng);
-			this.street_number = $(this.options.elements.street_number);
-			this.route = $(this.options.elements.route);
-			this.intersection = $(this.options.elements.intersection);
-			this.locality = $(this.options.elements.locality);
-			this.administrative_area_level_2 = $(this.options.elements.administrative_area_level_2);
-			this.administrative_area_level_1 = $(this.options.elements.administrative_area_level_1);
-			this.country  = $(this.options.elements.country);
-			this.postal_code = $(this.options.elements.postal_code);
-			this.type     = $(this.options.elements.type);
-			if (this.options.elements.map) {
+
+            // Long_names
+            this.street_number                  = $(this.options.elements.long_names.street_number);
+            this.route                          = $(this.options.elements.long_names.route);
+            this.intersection                   = $(this.options.elements.long_names.intersection);
+            this.locality                       = $(this.options.elements.long_names.locality);
+            this.political                      = $(this.options.elements.long_names.political);
+            this.administrative_area_level_3    = $(this.options.elements.long_names.administrative_area_level_3);
+            this.administrative_area_level_2    = $(this.options.elements.long_names.administrative_area_level_2);
+            this.administrative_area_level_1    = $(this.options.elements.long_names.administrative_area_level_1);
+            this.country                        = $(this.options.elements.long_names.country);
+            this.postal_code                    = $(this.options.elements.long_names.postal_code);
+            this.colloquial_area                = $(this.options.elements.long_names.colloquial_area);
+            this.sublocality                    = $(this.options.elements.long_names.sublocality);
+            this.neighborhood                   = $(this.options.elements.long_names.neighborhood);
+            this.premise                        = $(this.options.elements.long_names.premise);
+            this.subpremise                     = $(this.options.elements.long_names.subpremise);
+            this.natural_feature                = $(this.options.elements.long_names.natural_feature);
+            this.airport                        = $(this.options.elements.long_names.airport);
+            this.park                           = $(this.options.elements.long_names.park);
+            this.point_of_interest              = $(this.options.elements.long_names.point_of_interest);
+
+            // Short_names
+            this.street_number_s                  = $(this.options.elements.short_names.street_number);
+            this.route_s                          = $(this.options.elements.short_names.route);
+            this.intersection_s                   = $(this.options.elements.short_names.intersection);
+            this.locality_s                       = $(this.options.elements.short_names.locality);
+            this.political_s                      = $(this.options.elements.short_names.political);
+            this.administrative_area_level_3_s    = $(this.options.elements.short_names.administrative_area_level_3);
+            this.administrative_area_level_2_s    = $(this.options.elements.short_names.administrative_area_level_2);
+            this.administrative_area_level_1_s    = $(this.options.elements.short_names.administrative_area_level_1);
+            this.country_s                        = $(this.options.elements.short_names.country);
+            this.postal_code_s                    = $(this.options.elements.short_names.postal_code);
+            this.colloquial_area_s                = $(this.options.elements.short_names.colloquial_area);
+            this.sublocality_s                    = $(this.options.elements.short_names.sublocality);
+            this.neighborhood_s                   = $(this.options.elements.short_names.neighborhood);
+            this.premise_s                        = $(this.options.elements.short_names.premise);
+            this.subpremise_s                     = $(this.options.elements.short_names.subpremise);
+            this.natural_feature_s                = $(this.options.elements.short_names.natural_feature);
+            this.airport_s                        = $(this.options.elements.short_names.airport);
+            this.park_s                           = $(this.options.elements.short_names.park);
+            this.point_of_interest_s              = $(this.options.elements.short_names.point_of_interest);
+
+            // others
+            this.lat                            = $(this.options.elements.lat);
+            this.lng                            = $(this.options.elements.lng);
+            this.type                           = $(this.options.elements.type);
+
+            if (this.options.elements.map) {
 				this.mapElement = $(this.options.elements.map);
 				this._initMap();
 			}
@@ -129,11 +197,14 @@
 			})
 		},
 
-		_findInfo: function(result, type) {
-			for (var i = 0; i < result.address_components.length; i++) {
+		_findInfo: function(result, type, short_name) {
+            short_name = typeof short_name !== 'undefined' ? short_name : false;
+            for (var i = 0; i < result.address_components.length; i++) {
 				var component = result.address_components[i];
 				if (component.types.indexOf(type) !=-1) {
-					return component.long_name;
+					if(short_name === true)
+                        return component.short_name;
+                    return component.long_name;
 				}
 			}
 			return false;
@@ -152,7 +223,8 @@
 			}
 			this._updatePosition(address.geometry.location);
 		  
-			if (this.street_number) {
+			// Long names
+            if (this.street_number) {
 				this.street_number.val(this._findInfo(address, 'street_number'));
 			}
 			if (this.route) {
@@ -164,6 +236,9 @@
 			if (this.locality) {
 				this.locality.val(this._findInfo(address, 'locality'));
 			}
+            if (this.administrative_area_level_3) {
+                this.administrative_area_level_3.val(this._findInfo(address, 'administrative_area_level_3'));
+            }
 			if (this.administrative_area_level_2) {
 				this.administrative_area_level_2.val(this._findInfo(address, 'administrative_area_level_2'));
 			}
@@ -175,10 +250,101 @@
 			}
 			if (this.postal_code) {
 				this.postal_code.val(this._findInfo(address, 'postal_code'));
-			}			
-			if (this.type) {
-				this.type.val(address.types[0]);
 			}
+            if (this.political) {
+                this.political.val(this._findInfo(address, 'political'));
+            }
+            if (this.colloquial_area) {
+                this.colloquial_area.val(this._findInfo(address, 'colloquial_area'));
+            }
+            if (this.sublocality) {
+                this.sublocality.val(this._findInfo(address, 'sublocality'));
+            }
+            if (this.neighborhood) {
+                this.neighborhood.val(this._findInfo(address, 'neighborhood'));
+            }
+            if (this.premise) {
+                this.premise.val(this._findInfo(address, 'premise'));
+            }
+            if (this.subpremise) {
+                this.subpremise.val(this._findInfo(address, 'subpremise'));
+            }
+            if (this.natural_feature) {
+                this.natural_feature.val(this._findInfo(address, 'natural_feature'));
+            }
+            if (this.airport) {
+                this.airport.val(this._findInfo(address, 'airport'));
+            }
+            if (this.park) {
+                this.park.val(this._findInfo(address, 'park'));
+            }
+            if (this.point_of_interest) {
+                this.point_of_interest.val(this._findInfo(address, 'point_of_interest'));
+            }
+
+            // Short names
+            if (this.street_number_s) {
+                this.street_number_s.val(this._findInfo(address, 'street_number', true));
+            }
+            if (this.route_s) {
+                this.route_s.val(this._findInfo(address, 'route', true));
+            }
+            if (this.intersection_s) {
+                this.intersection_s.val(this._findInfo(address, 'intersection', true));
+            }
+            if (this.locality_s) {
+                this.locality_s.val(this._findInfo(address, 'locality', true));
+            }
+            if (this.administrative_area_level_3_s) {
+                this.administrative_area_level_3_s.val(this._findInfo(address, 'administrative_area_level_3', true));
+            }
+            if (this.administrative_area_level_2_s) {
+                this.administrative_area_level_2_s.val(this._findInfo(address, 'administrative_area_level_2', true));
+            }
+            if (this.administrative_area_level_1_s) {
+                this.administrative_area_level_1_s.val(this._findInfo(address, 'administrative_area_level_1', true));
+            }
+            if (this.country_s) {
+                this.country_s.val(this._findInfo(address, 'country', true));
+            }
+            if (this.postal_code_s) {
+                this.postal_code_s.val(this._findInfo(address, 'postal_code', true));
+            }
+            if (this.political_s) {
+                this.political_s.val(this._findInfo(address, 'political', true));
+            }
+            if (this.colloquial_area_s) {
+                this.colloquial_area_s.val(this._findInfo(address, 'colloquial_area', true));
+            }
+            if (this.sublocality_s) {
+                this.sublocality_s.val(this._findInfo(address, 'sublocality', true));
+            }
+            if (this.neighborhood_s) {
+                this.neighborhood_s.val(this._findInfo(address, 'neighborhood', true));
+            }
+            if (this.premise_s) {
+                this.premise_s.val(this._findInfo(address, 'premise', true));
+            }
+            if (this.subpremise_s) {
+                this.subpremise_s.val(this._findInfo(address, 'subpremise', true));
+            }
+            if (this.natural_feature_s) {
+                this.natural_feature_s.val(this._findInfo(address, 'natural_feature', true));
+            }
+            if (this.airport_s) {
+                this.airport_s.val(this._findInfo(address, 'airport', true));
+            }
+            if (this.park_s) {
+                this.park_s.val(this._findInfo(address, 'park', true));
+            }
+            if (this.point_of_interest_s) {
+                this.point_of_interest_s.val(this._findInfo(address, 'point_of_interest', true));
+            }
+
+            // Others
+            if (this.type) {
+                this.type.val(address.types[0]);
+            }
 		},
 
 		_selectAddress: function(event, ui) {
